37fa6436ee0c5d9ebbf8088da473c9cf12a69752
Fareor <127655715+Fareor@users.noreply.github.com>
2023-11-22 20:43:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,16 @@ intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command()
-async def список(ctx):
+async def проблемы(ctx):
     await ctx.send('Причины глобального потепления:\n'
-    '1.')
+    '1. Проблема 1: Выбросы парниковых газов\n'
+   '- Решение: Переход на возобновляемые источники энергии,\n такие как солнечная и ветровая энергия, \nчтобы сократить зависимость от ископаемых и \nуменьшить выбросы парниковых газов.'
 
+    '2. Проблема 2: Загрязнение атмосферы'
+    '- Решение: Использование чистых технологий \nи процессов производства, \nчтобы уменьшить выбросы вредных веществ в \nатмосферу и повысить качество воздуха.')
+@bot.command()
+async def помощь(ctx):
+    await ctx.send('В проблеме с ГП вам помогут сайты:\n'
+                   '1. https://www.un.org/ru/global-issues/climate-change \n'
+                   '2. https://habr.com/ru/articles/560378/ ')
 bot.run("YOUR_TOKEN")
